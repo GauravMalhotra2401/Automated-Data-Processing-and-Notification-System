bdc6141683934760541b3e1b85b86f18c11c9c0f
Gaurav Malhotra <malhotragaurav371@gmail.com>
2024-06-04 21:53:44 +0530

    Added loggers to identify if my consumer lambda is receiving messages

--- lambda_function_consumer.py
+++ lambda_function_consumer.py
@@ -1,31 +1,42 @@
 import json
 import boto3
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)  # Log all messages for debugging
+
+sqs_client = boto3.client('sqs')
+sqs_queue_url = "https://sqs.us-east-1.amazonaws.com/058264373160/movie-recommendation-queue"
 
 def lambda_handler(event, context):
-
-    sqs_client = boto3.client('sqs')
-    sqs_queue_url = "https://sqs.us-east-1.amazonaws.com/058264373160/movie-recommendation-queue"
+    logger.debug(f"Event: {event}")  # Log the entire event object
+    logger.debug(f"Context: {context}")  # Log the context object
 
     response = sqs_client.receive_message(
-        QueueUrl = sqs_queue_url,
-        MaxNumberOfMessages = 1,
-        WaitTimeSeconds = 10
+        QueueUrl=sqs_queue_url,
+        MaxNumberOfMessages=1,
+        WaitTimeSeconds=10
     )
 
     if 'Messages' in response:
         for message in response['Messages']:
             body = json.loads(message['Body'])
+            logger.debug(f"Received Message Body: {body}") 
 
-            print(f"Received Movie data : {body}")
+            try:
+                receipt_handle = message['ReceiptHandle']
+                logger.debug(f"Receipt Handle: {receipt_handle}")
 
-            receipt_handle = message['ReceiptHandle']
-            sqs_client.delete_message(
-                QueueUrl = sqs_queue_url,
-                ReceiptHandle = receipt_handle
-            )
+                sqs_client.delete_message(
+                    QueueUrl=sqs_queue_url,
+                    ReceiptHandle=receipt_handle
+                )
+                logger.debug(f"Message deleted successfully")
+
+            except Exception as e:
+                logger.error(f"Error deleting message: {e}")
     else:
-        print("No messages received")
+        logger.info("No messages received")
 
     return {
         'statusCode': 200,
